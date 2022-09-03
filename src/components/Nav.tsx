ef95cdec35718503cf5821b7c467da7fe3b77717
kitan23 <ktran@hamilton.edu>
2022-09-02 21:31:33 -0400

    change order nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export const Nav: React.FC = () => {
 	return (
 		<NavStyle>
-			<Link style={{ textDecoration: "none", color: "black" }} to="/home">
-				Home
-			</Link>
 			<Link style={{ textDecoration: "none", color: "black" }} to="/">
 				About
+			</Link>
+			<Link style={{ textDecoration: "none", color: "black" }} to="/home">
+				Home
 			</Link>
 		</NavStyle>
 	);
